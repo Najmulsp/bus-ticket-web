8f4cc79bc2ca80445aebf5ae557a7e0ff75a03cb
Najmulsp <mdnajmulsarker1595@gmail.com>
2024-02-18 23:22:07 +0600

    seat selecting now

--- bus-ticket.js
+++ bus-ticket.js
@@ -1,12 +1,20 @@
-
-
-function getSelectedSeat(){
-
+let count =40;
+let increase =0;
+const seats =document.getElementsByClassName('chair');
+for(const seat of seats){
+    seat.addEventListener("click", function(){
+        seat.classList.add('bg-green-500');
+        
+        count =count-1;
+        document.getElementById('remain-seats').innerText=count;
+        
+        increase =increase + 1;
+        document.getElementById('increase-selected').innerText=increase;
+        
+    })
 }
+
 
 function chooseTicket(){
-    showElementById('ticketing');
-    getSelectedSeat()
+    showElementById('ticketing');   
 }
-   
-    
